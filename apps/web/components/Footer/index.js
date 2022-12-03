b324ab27b9c22c5019137dc77f4346fe26fe7797
Furkan <furkanktnci@gmail.com>
2022-12-04 02:44:54 +0300

    footer menu created

--- apps/web/components/Footer/index.js
+++ apps/web/components/Footer/index.js
@@ -1,7 +1,81 @@
+import styles from "./footer.module.css";
+
+const footerData = {
+  allItems: [
+    {
+      title: "SİTE HARİTASI",
+      items: [
+        {
+          text: "SNEAKER",
+        },
+        {
+          text: "GİYİM",
+        },
+        {
+          text: "AKSESUAR",
+        },
+        {
+          text: "YENİLER",
+        },
+        {
+          text: "İNDİRİM",
+        },
+      ],
+    },
+    {
+      title: "KURUMSAL",
+      items: [
+        {
+          text: "SNEAKER",
+        },
+        {
+          text: "GİYİM",
+        },
+        {
+          text: "AKSESUAR",
+        },
+        {
+          text: "YENİLER",
+        },
+        {
+          text: "İNDİRİM",
+        },
+      ],
+    },
+    {
+      title: "HESABIM",
+      items: [
+        {
+          text: "SNEAKER",
+        },
+        {
+          text: "GİYİM",
+        },
+        {
+          text: "AKSESUAR",
+        },
+        {
+          text: "YENİLER",
+        },
+        {
+          text: "İNDİRİM",
+        },
+      ],
+    }
+  ]
+};
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footerContainer}>
+      <div className={styles.footerWrapper}>
+        {footerData.allItems.map((item, index) => (
+          <>     
+          <div className={styles.titles} key={index}>{item.title}</div>
+           </>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- apps/web/components/layout.js
+++ apps/web/components/layout.js
@@ -1,12 +1,24 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 
 export default function Layout({ children }) {
-    return (
-      <>
-        <Navbar />
-        <main>{children}</main>
-        {/* <Footer /> */}
-      </>
-    )
-  }
+  const [height, setHeight] = useState(0)
+
+  
+  useEffect(() => {
+    if(window) {
+        const { innerHeight: height } = window;
+        // console.log("height" , height)
+        setHeight(height)
+    }
+}, [])
+
+  return (
+    <>
+      <Navbar />
+      <div style={{minHeight: "calc(100vh - 60px)"}}>{children}</div>
+      <Footer />
+    </>
+  );
+}
