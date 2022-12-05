aee5bc04bf0e53750a6834aced639343a5676d0a
Kerim Can Gökdağ <kgokdagg@gmail.com>
2022-12-05 23:52:10 +0300

    added user login,register

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,46 +1,61 @@
 var express = require("express");
 var router = express.Router();
-const { Users } = require("../dbSchemas/firebase");
-const admin = require("firebase-admin");
+const { getFirestore } = require("firebase-admin/firestore");
+const db = getFirestore();
+const { SignJWT } = require("jose");
+require("dotenv").config();
+const { v4 } = require("uuid");
 
-router.get("/getAllUser", async function(req, res, next) {
+router.post("/login", async function(req, res, next) {
+  const { MAIL, PASSWORD } = req.body;
   try {
-
-    const checkresult = (await Users.get()).data();
-
-    return res.json({ status: true, data: checkresult });
+    if (!MAIL || !PASSWORD) return res.status(400).send({ status: false, errorMessage: "Bad request" });
+    const [checkUser] = (await db.collection("user").limit(1).where("mail", "==", MAIL).where("password", "==", PASSWORD).get()).docs;
+    if (checkUser) {
+      const token = await new SignJWT({
+        name: checkUser.name,
+        favorite: checkUser.favoriteProducts,
+        lastName: checkUser.lastName,
+        mail: checkUser.mail,
+        userId: checkUser.userId
+      }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("24h").sign(new TextEncoder().encode(process.env.JWT_KEY));
+      return res.json({ status: true, data: { ...checkUser.data(), token } });
+    }
+    return res.json({ status: true, data: null });
   } catch (e) {
-    return res.status(500).send({ status: false, errorMessage: "Server Internal Error" });
+    return res.status(500).send({ status: false, errorMessage: "Internal server error" });
   }
 });
 
-router.post("/setUser", async function(req, res, next) {
+router.get("/register", async (req, res) => {
+  const { NAME, LASTNAME, MAIL, PASSWORD } = req.query;
   try {
-    const { NAME, LASTNAME, MAIL, PASSWORD, FAVORITES } = req.body;
+    const [checkUser] = (await db.collection("user").limit(1).where("mail", "==", MAIL).get()).docs;
+    if (checkUser) return res.json({ status: false, errorMessage: "E-mail address already exists" });
+    const randomDocId = v4();
+    const docRef = await db.collection("user").doc(randomDocId);
 
-    if (!NAME || !LASTNAME || !MAIL || !PASSWORD) return res.status(400).send({
-      status: false,
-      errorMessage: "Bad request"
+    const token = await new SignJWT({
+      name: NAME,
+      favorite: [],
+      lastName: LASTNAME,
+      mail: MAIL,
+      password: PASSWORD,
+      userId: randomDocId
+    }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("24h").sign(new TextEncoder().encode(process.env.JWT_KEY));
+
+    await docRef.set({
+      name: NAME,
+      favorite: [],
+      lastName: LASTNAME,
+      mail: MAIL,
+      userId: randomDocId
     });
+    return res.json({ status: true, data: token });
 
-    if (FAVORITES && !FAVORITES.length) return res.status(400).send({ status: false, errorMessage: "Bad request" });
-
-    const checkresult = await Users.update({
-      users: admin.firestore.FieldValue.arrayUnion({
-        favoriteProducts: FAVORITES,
-        lastName: LASTNAME,
-        name: NAME,
-        mail: MAIL,
-        password: PASSWORD,
-        userId: Math.random().toFixed(4)
-      })
-    });
-
-    return res.json({ status: true, data: checkresult });
   } catch (e) {
-    return res.status(500).send({ status: false, errorMessage: "Server Internal Error" });
+    return res.status(500).send({ status: false, errorMessage: "Internal server error" });
   }
 });
-
 
 module.exports = router;
