d2305131b496398551de954336535a1827dda15d
Kerim Can Gökdağ <kgokdagg@gmail.com>
2022-12-07 21:52:38 +0300

    cors

--- server/api/app.js
+++ server/api/app.js
@@ -29,9 +29,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/usercrud", indexRouter);
-app.use("/api",checkTokenFunc);
-app.use("/api/product",userRouter);
+app.use("/api", checkTokenFunc);
+app.use("/api/product", userRouter);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
