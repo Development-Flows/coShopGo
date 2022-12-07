874d0234a72ec32c7e79b800f17c7ab993e52dfe
Kerim Can Gökdağ <kgokdagg@gmail.com>
2022-12-07 22:13:11 +0300

    added library cors

--- server/api/app.js
+++ server/api/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const checkTokenFunc = require("./middlewares");
+const cors = require("cors");
 
 /*Firebase*/
 var serviceAccount = require("./service/coshopgo-firebase-adminsdk-t2ssx-dc90bef8d6.json");
@@ -37,6 +38,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -45,7 +47,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/usercrud", indexRouter);
 app.use("/api", checkTokenFunc);
 app.use("/api/product", userRouter);
-
 
 
 // catch 404 and forward to error handler
